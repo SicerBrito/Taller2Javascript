aaed58a82e5dc6c79ba06b6afc97c27963ce212d
SicerAndresBritoGutierrezCAMPUS <britodelgado514@gmail.com>
2023-05-03 10:47:55 -0500

    feat: :sparkles: Ejercicio 4
    Realize el cuarto ejercicio del taller numero 2 de Javascript

--- main.js
+++ main.js
@@ -1,21 +1,39 @@
-/* Ejercicio #3 Taller 2 Javascript 
-3. Construir el algoritmo para determinar el voltaje de un
-circuito a partir de la resistencia y la intensidad de corriente.*/
-
-/* E = I x R. */
+/* Ejercicio #4 Taller 2 Javascript 
+Construir el algoritmo que solicite el nombre y edad de 3
+personas y determine el nombre de la persona con mayor edad.*/
 
 let seguir = true;
 
 do {
-    
-let intensidad = prompt("Ingrese la intencidad en amperios.");
-let resistencia = prompt("Ingrese la resistencia en ohmios.");
+alert("Digite 3 nombres diferentes junto a su edad correspondiente.");
+let nombre1 = prompt("Ingrese un nombre.");
+let edad1 = parseFloat(prompt(`Ingrese la edad de ${nombre1}.`));
+let nombre2 = prompt("Ingrese un nombre.");
+let edad2 = parseFloat(prompt(`Ingrese la edad de ${nombre2}.`));
+let nombre3 = prompt("Ingrese un nombre.");
+let edad3 = parseFloat(prompt(`Ingrese la edad de ${nombre3}.`));
 
-let voltaje = (intensidad * resistencia);
 
-alert(`El voltaje es ${voltaje} voltios`);
 
+let nombreMayorEdad = "";
+if (edad1 > edad2 && edad1 > edad3) {
+  nombreMayorEdad = nombre1;
+  mayorEdad = edad1;
+
+} else if (edad2 > edad1 && edad2 > edad3) {
+  nombreMayorEdad = nombre2;seguir = confirm("Desea ingresar otro número?");
+
+  mayorEdad = edad2;
+
+} else {
+  nombreMayorEdad = nombre3;
+  mayorEdad = edad3;
+
+}
+
+alert(`La persona con mayor edad es: ${nombreMayorEdad} con ${mayorEdad} años`);
 
 seguir = confirm("Desea ingresar mas datos?");
+
 } while (seguir);
 
